7f4087a73b411fc942d1a3e8320a7af76b1c7890
yavuz <yavuz.akpinar.68@gmail.com>
2021-11-21 22:58:51 +0300

    Tasarım için gerekli componentler oluşturuldu

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+
 
 const App = () => {
   return(
