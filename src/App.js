0146330e29c033cc3e5dd2b1e31481b9c8e79c70
yavuz <yavuz.akpinar.68@gmail.com>
2021-11-22 23:20:52 +0300

    feat: Alert uyarısı düzeltildi,SButton componenti için onPress eventi denendi.
    style: componentler gerekli viewlerle sarılarak flex yapısı oluşturuldu.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { Button, FlatList, SafeAreaView, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {FlatList, SafeAreaView, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import Input from './Components/Input/Input';
+import Sbutton from './Components/SıralamaButonu/SButton';
 import UrunCard from './Components/UrunCard/UrunCard';
 
 
@@ -15,29 +16,43 @@ const App = () => {
     urun ? fiyat ?
     setSatıs([{urun:urun, fiyat:fiyat},...satıs]):
     alert("Urun veya Fiyat özellikleri boş bırakılamaz"):
-    alert("Urun veya Fiyat özellikleri boş bırakılamaz")
+    alert("Urun veya Fiyat özellikleri boş bırakılamaz");
+    setFiyat()
+    setUrun("")
+  }
+  const azalan = () => {
+    satıs.sort((a,b) => a.fiyat.localeCompare(b.fiyat))
   }
  
   return(
-    <SafeAreaView>
-      <FlatList 
-        data={satıs} 
-        renderItem={({item}) => <UrunCard item={item}/>}/>
-      <Input 
-        placeholder="Urun..." 
-        title="Urun Adı" 
-        value={urun} 
-        onChangeText={setUrun}></Input>
-      <Input 
-        placeholder="Fiyat..." 
-        title="Fiyat" 
-        value={fiyat} 
-        onChangeText={setFiyat}></Input>
-      <TouchableOpacity
-        style={styles.buttonContainer}
-        onPress={addUrun}> 
-        <Text style={styles.buttonText}>Ekle</Text>
-      </TouchableOpacity>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.sButtonView}>
+        <Sbutton title="Artan" />
+        <Sbutton title="Azalan" onPress={azalan}/>
+        <Sbutton title="Tarih"/>
+      </View>
+      <View style={styles.flatListView}>
+        <FlatList 
+          data={satıs} 
+          renderItem={({item}) => <UrunCard item={item}/>}/>
+      </View>
+      <View style={styles.inputView}>
+        <Input 
+          placeholder="Urun..." 
+          title="Urun Adı" 
+          value={urun} 
+          onChangeText={setUrun}></Input>
+        <Input 
+          placeholder="Fiyat..." 
+          title="Fiyat" 
+          value={fiyat} 
+          onChangeText={setFiyat}></Input>
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={addUrun}> 
+          <Text style={styles.buttonText}>Ekle</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   )
 }
@@ -54,5 +69,19 @@ const styles = StyleSheet.create({
   },
   buttonText:{
     fontSize:15
+  },
+  sButtonView:{
+    flexDirection:"row",
+    justifyContent:"space-between",
+    flex:1
+  },
+  flatListView:{
+    flex:8
+  },
+  container:{
+    flex:1
+  },
+  inputView:{
+    flex:4,
   }
 })
